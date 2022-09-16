880907f345eedc00d9a6f1de30968a4574f0ee99
RicardoTejedorRestrepo <103795653+RicardoTejedorRestrepo@users.noreply.github.com>
2022-09-15 22:38:57 -0500

    Update teclado.widgets.dart

--- lib/widgets/teclado.widgets.dart
+++ lib/widgets/teclado.widgets.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +11,16 @@ Widget Teclado(List<BotonModel> lista){
   lista.forEach((element){
     listaBotones.add(ElevatedButton(
                         onPressed: element.metodo, 
-                        child: Text(element.titulo, style: TextStyle(fontSize: 30,),)));
+                        style: TextButton.styleFrom(
+                                primary: Color.fromARGB(255, 0, 0, 0),
+                                backgroundColor: Color.fromARGB(255, 255, 0, 0),
+                                elevation: 10,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(32.0)),
+                                minimumSize: Size(100, 40),
+                                  ),
+                        child: Text(element.titulo, style: TextStyle(fontSize: 30,
+                                                            fontWeight: FontWeight.bold,),)));
   });
 
   return Row(
